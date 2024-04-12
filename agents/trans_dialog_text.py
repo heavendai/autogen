8a2b2b74368d3e11e2919a137ac74688f16c1152
heavendai <mingyang.heaven@gmail.com>
2024-04-12 17:16:16 +0800

    update trans_dialog_text.py

--- agents/trans_dialog_text.py
+++ agents/trans_dialog_text.py
@@ -28,18 +28,21 @@ from langchain_core.callbacks import BaseCallbackHandler
 from langchain_community.llms import QianfanLLMEndpoint
 
 from typing import Any, Dict, List, Union
+from tqdm import tqdm
+import logging
 
-"""
+logging.basicConfig(filename='trans.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+#logger = loggin.getLogger(__name__)
+
+UNIT_LINE_COUNT = 60
+
 model = QianfanLLMEndpoint(
         streaming=True,
-        model="ERNIE-Bot-4",
-        temperature=0.7,
-        max_tokens=20
+        model="ERNIE-3.5-8K",
+        temperature=0.1,
+        #max_tokens=50
     )
 
-"""
-#ret1 = model.invoke("""请把下面的对话，转成更合理的语言对话，请一定保证句子标点符号的正确性。并用很口语化的生活用语表述来改写对话内容。
-#对话如下：{input}""")
 
 def chunk_split(lst, M):
     # 使用列表推导和range()按M切片列表
@@ -48,14 +51,36 @@ def chunk_split(lst, M):
 def main(src_file, dst_file):
     lines = open(src_file, 'r').readlines()
 
-    chunk_lst = chunk_split(lines, 40)
-    print(chunk_lst)
-    #fp_dst = open(dst_file, 'w')
-    #fp_dst.close()
+    chunk_lst = chunk_split(lines, UNIT_LINE_COUNT)
+    fp_log = open("trans_dialog_g3.log", 'w')
+    fp_dst = open(dst_file, 'w')
+    fp_last = open("last.log", 'a')
+    logging.warning(len(chunk_lst))
+    #for term in chunk_lst:
+    for i in tqdm(range(len(chunk_lst))):
+        term = chunk_lst[i]
+        logging.warning(f"i:{i}, {len(chunk_lst)}\n")
+        content = "".join(term)
+        human_input = f"""请把下面的对话，转成更合理的语言对话，请一定保证句子标点符号的正确性。并用很口语化的生活用语表述来改写对话内容。
+对话如下：```{content}```"""
+        fp_log.write(human_input + '\n')
+        try:
+            ret = model.invoke(human_input)
+        except Exception as ex:
+            fp_last.write(content + '\n')
+            continue
+
+        fp_dst.write(ret + '\n')
+        fp_log.write("========\n")
+
+    fp_last.close()
+    fp_dst.close()
+    fp_log.close()
     return 
 
 if __name__ == "__main__":
-    src_file = r''
-    dst_file = r''
+    src_file = r'/data1/mingyang/data/dl/concate_info.op.scp.g3'
+    dst_file = r'/data1/mingyang/data/dl/concate_info.out.scp.g3'
+
     main(src_file, dst_file)
 
